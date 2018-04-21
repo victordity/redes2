74c2fc8dc2fdde7b0c7e185c696dcd5be6fae75a
Dity <victordity@gmail.com>
2018-04-21 10:51:32 -0300

    atualizacao

--- client.py
+++ client.py
@@ -1,15 +1,23 @@
-from socket import *
+#from socket import *
 import sys
 
+
+def encode16(message):
+    encoded = binascii.hexlify(bytes(message, "utf-8"))
+    encoded = str(encoded).strip("b")
+    encoded = encoded.strip("")
+    return encoded
 
 
 host = '127.0.0.1'
 server_port = 5152
-s = socket(AF_INET, SOCK_STREAM)
+# s = socket(AF_INET, SOCK_STREAM)
 dest = (host, server_port)
-s.connect(dest)
+# s.connect(dest)
 
 msg = ("Mensagem de teste")
-msgb = msg.encode('utf-8')
-s.send(msgb)
-s.close()
+# msgb = msg.encode('utf-8')
+msgb16 = encode16(msg)
+print(msgb16)
+# s.send(msgb16)
+# s.close()
