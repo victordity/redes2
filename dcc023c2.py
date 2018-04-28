4ee9d075875c310692055eb9ff115171d853beda
franciscds <franciscarlossantos@gmail.com>
2018-04-28 17:51:19 -0300

    c

--- dcc023c2.py
+++ dcc023c2.py
@@ -1,16 +1,19 @@
 import sys
+import socket
 
 
 def emuladorServer(SERVER_PORT,INPUT,OUTPUT):
        # SERVER_PORT = argv[1]
-    host = '127.0.0.1'
+    HOST = '127.0.0.1'
 
-    s = socket(socket.AF_INET, socket.SOCK_STREAM,0)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM,0)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(HOST, PORT)
-    orig = (host, SERVER_PORT)
+    PORT = int(SERVER_PORT)
+    orig = (HOST, PORT)
+    s.bind((HOST, PORT))
+  
 
-    s.listen(1)
+    s.listen(10)
     print("Esperando conexao")
     while True:
         con, cliente = s.accept()
@@ -23,50 +26,69 @@ def emuladorServer(SERVER_PORT,INPUT,OUTPUT):
 def conectado(con, cliente):
     print('Conectado por', cliente)
 
+    # while True:
+    #     quadro16 = con.recv(1024)
+    #     quadro = utils.decode16(quadro16)
+    #     sync = 'dcc023c2dcc023c2'
+    #     # Confirmar o sync
+    #     if (quadro[0:16] == sync):
+    #         tamQuadro = len(quadro)
+    #         length = quadro[17:20]
+    #         dado = quadro[(tamQuadro-length):tamQuadro]
+    #         checksum = utils.checksum(dado)
+
+    #     else:
+    #         pass
+
+
+    #     if not msg: break
+    #     print(cliente, msg)
     while True:
-        quadro16 = con.recv(1024)
-        quadro = utils.decode16(quadro16)
-        sync = 'dcc023c2dcc023c2'
-        # Confirmar o sync
-        if (quadro[0:16] == sync):
-            tamQuadro = len(quadro)
-            length = quadro[17:20]
-            dado = quadro[(tamQuadro-length):tamQuadro]
-            checksum = utils.checksum(dado)
-
-        else:
-            pass
-
-
-        if not msg: break
-        print(cliente, msg)
-
+        data = con.recv(1024)
+        if not data:
+            break
+    msg = 'Recebi: ' + data
+    con.send(msg)
+    print(data)
     print('Finalizando conexao do cliente', cliente)
     con.close()
     thread.exit()
 
 
 def emuladorClient(IP,SERVER_PORT,INPUT,OUTPUT):
-host = '127.0.0.1'
+    host = '127.0.0.1'
 
-# SERVER_PORT = sys.argv[1]
+    SERVER = int(SERVER_PORT)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM,0)
+    dest = (IP, SERVER_PORT)
+    s.connect((host,SERVER))
+    message = 'my pistol'
+    
+    while True:
+        s.send(message.encode('ascii'))
 
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM,0)
-dest = (IP, SERVER_PORT)
-# s.connect(dest)
+        msg = s.recv(1024) 
+        print('Recebido:',data.decode(ascii))
+        ans = input('\n Quer continuar?')
+        if ans == 'y':
+            continue
+        else:
+            break
+    s.close()
 # fileName = sys.argv[2]
-fileName = 'teste.txt'
-quadros = enquadramento(fileName)
+#fileName = 'teste.txt'
+#quadros = enquadramento(fileName)
 # msg = sys.argv[3]
-msg = 'ABC'
-mb16 = utils.encode16(msg)
+# msg = 'ABC'
+# mb16 = utils.encode16(msg)
 # msgb = msg.encode('utf-8')
-print('A mensagem a ser enviada eh: {}'.format(mb16))
+# print('A mensagem a ser enviada eh: {}'.format(mb16))
 # s.close()
+    
 
+flag = sys.argv[1]
+if flag == '-s':
+    emuladorServer(sys.argv[2],sys.argv[3],sys.argv[4])
 
-flag = sys.argv[1];
-if flag == 's':
+else:
     emuladorClient(sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
-    else:
-        emuladorServer(sys.argv[2],sys.argv[3],sys.argv[4]);
